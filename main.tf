c1df4a0aa0f1fa150baa07c64b8cc094d08a3a7a
Christopher Markieta <markietachristopher@gmail.com>
2020-06-30 09:10:50 -0400

    Adding nonprod env.

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "remote" {
     hostname = "tfe.markieta.io"
-    organization = "dev"
+    organization = "nonprod"
 
     workspaces {
-      name = "vm-dev"
+      name = "vm-nonprod"
     }
   }
 }
