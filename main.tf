24996e215c974bf24306743775a4d5dd2cfe870f
Christopher Markieta <markietachristopher@gmail.com>
2020-06-30 08:14:11 -0400

    Reverting back to no remote.

--- main.tf
+++ main.tf
@@ -1,10 +1,3 @@
-terraform {
-  backend "remote" {
-    hostname = "tfe.markieta.io"
-    organization = "Arctiq-Machines"
-  }
-}
-
 resource "null_resource" "hello" {
   provisioner "local-exec" {
     command = "echo Hello World!"
