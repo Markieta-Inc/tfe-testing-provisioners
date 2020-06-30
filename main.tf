777c3c3d23e4c9a70f2b1720617a36bc32c66e1e
Christopher Markieta <markietachristopher@gmail.com>
2020-06-29 23:52:43 -0400

    Adding remote state.

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "remote" {
+    hostname = "tfe.markieta.io"
+    organization = "Arctiq-Machines"
+  }
+}
+
 resource "null_resource" "hello" {
   provisioner "local-exec" {
     command = "echo Hello World!"
